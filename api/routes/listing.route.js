d450cab5eb4df72a827831d617dd1c6bf70154f7
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-11-04 05:23:41 +0530

    complete search  api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import {createlisting,deletelisting,updatelisting,getListing} from '../controllers/listing.controller.js';
+import {createlisting,deletelisting,updatelisting,getListing,getListings} from '../controllers/listing.controller.js';
 const router = express.Router();
 
 router.post('/create', verifyToken ,createlisting);
 router.delete('/delete/:id', verifyToken ,deletelisting);
 router.post('/update/:id', verifyToken ,updatelisting);
 router.get('/get/:id',getListing);
+router.get('/get', getListings);
 export default router;

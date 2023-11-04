437272944a1784fd33595277caf119b43c66e4a5
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-11-05 03:41:52 +0530

    minor changes

--- client/src/components/PrivateRoute.tsx
+++ client/src/components/PrivateRoute.tsx
@@ -1,11 +1,10 @@
 import { useSelector } from "react-redux";
-import{Outlet, Navigate} from 'react-router-dom';
+import { Outlet, Navigate } from "react-router-dom";
 const PrivateRoute = () => {
-     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-    const{currentUser} = useSelector((state) => state.user);
-  return currentUser ? <Outlet/> : <Navigate to='/sign-in'/>
-   
-}
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  const { currentUser } = useSelector((state) => state.user);
+  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+};
 
 export default PrivateRoute;

b75cd96bf20a243e4b78a3c3f207f69c915a7e3e
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-12-04 20:55:20 +0530

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- 
-
   plugins: [react()],
 });
-https://real-estate-backend-mu.vercel.app

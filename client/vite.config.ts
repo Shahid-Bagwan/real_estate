64904828d5f81a5de48262b70603156d36f74ff7
Shahid Bagwan <112220551+Shahid-Bagwan@users.noreply.github.com>
2023-12-04 20:08:10 +0530

    frontend deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
+      "/api": {
+        target: "https://real-estate-backend-mu.vercel.app",
         secure: false,
       },
     },
   },
-  
+
   plugins: [react()],
 });
